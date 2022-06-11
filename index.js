9232f0bbdd4547c0aca41f2b547e6571cab1fbf1
anishjoshi1999 <anishjoshi1999@gmail.com>
2022-06-11 10:09:02 +0545

    Implement uuidv4 in new comments

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
+const { v4: uuidv4 } = require('uuid');
+
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
@@ -10,19 +12,19 @@ app.set('view engine','ejs');
 
 // Comments
 const comments = [
-{	id:1,
+{	id:uuidv4(),
 	username:"Anish",
 	comment:'lol that is so funny'
 },
-{	id:2,
+{	id:uuidv4(),
 	username:"Pradip",
 	comment:'love from nepal'
 },
-{	id:3,
+{	id:uuidv4(),
 	username:"Roshan",
 	comment:'I liked it'
 },
-{	id:4,
+{	id:uuidv4(),
 	username:"Mahendra",
 	comment:'when are you getting married'
 }
@@ -40,14 +42,14 @@ app.get('/comments/new',(req,res)=>{
 // appending data from form into our database
 app.post('/comments',(req,res)=>{
 	const {username,comment} = req.body
-	comments.push({username,comment})
+	comments.push({id:uuidv4(),username,comment})
 	res.redirect('/comments');
 })
 // to show to the details of comment
 app.get('/comments/:id',(req,res)=>{
 	const {id} = req.params
 	// searching comment through id
-	const foundComment = comments.find((comment) => comment.id === parseInt(id))
+	const foundComment = comments.find((comment) => comment.id === id)
 	res.render('comments/show.ejs',{foundComment})
 })
 
